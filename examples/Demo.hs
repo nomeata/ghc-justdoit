dd288f0dc6330ce61c2888055f2a9426787316ee
Joachim Breitner <mail@joachim-breitner.de>
2018-02-02 14:59:57 -0500

    More examples

--- examples/Demo.hs
+++ examples/Demo.hs
@@ -2,7 +2,7 @@
 {-# OPTIONS_GHC -fplugin=Test.Inspection.Plugin #-}
 {-# LANGUAGE TemplateHaskell, LambdaCase, EmptyCase #-}
 import GHC.JustDoIt
-import Test.Inspection
+import Test.Inspection (inspect, (===), (==-))
 
 import Prelude hiding (id, flip, const, curry)
 
@@ -12,6 +12,8 @@ data Unit = Unit
 data Void
 data MyLargeSum a b c d e = MkA a | MkB b | MkC c | MkD d | MkE e
 newtype Id a = Id a
+data Result a b = Failure a | Success b
+newtype ErrRead r e a = ErrRead { unErrRead :: r -> Result e a }
 
 -- All these functions have basically one sensible implementation.
 -- With GHC.JustDoIt, we don’t have to write them.
@@ -45,6 +47,21 @@ curryFlip = (…)
 
 contBind :: ((a -> r) -> r) -> (a -> ((b -> r) -> r)) -> ((b -> r) -> r)
 contBind = (…)
+
+contAp :: (((a -> b) -> r) -> r) -> ((a -> r) -> r) -> ((b -> r) -> r)
+contAp = (…)
+
+errReadBind :: (r -> Either e a) -> (a -> (r -> Either e b)) -> (r -> Either e b)
+errReadBind = (…)
+
+errReadBindTup :: (r -> Either e a) -> (a -> (r -> Either e b)) -> (r -> Either e (a,b))
+errReadBindTup = (…)
+
+errReadAp :: (r -> Either e (a -> b)) -> (r -> Either e a) -> (r -> Either e b)
+errReadAp = (…)
+
+errReadBindTup2 :: ErrRead r e a -> (a -> ErrRead r e b) -> ErrRead r e (a,b)
+errReadBindTup2 = (…)
 
 unit :: Unit
 unit = (…)
@@ -80,6 +97,7 @@ proj' (_,_,c,_) = c
 curry' f a b = f (a,b)
 curryFlip' f a b = f (b,a)
 unit' = Unit
+mapId' f (Id x) = Id (f x)
 contBind' :: ((a -> r) -> r) -> (a -> ((b -> r) -> r)) -> ((b -> r) -> r)
 contBind' ca cb k = ca (\a -> cb a k)
 swapEither' (Left a) = (Right a)
@@ -89,26 +107,51 @@ swapEitherCont' ca k = ca $ \case Left a -> k (Right a)
                                   Right a -> k (Left a)
 absurd' :: Void -> a
 absurd' = \case{}
-mapId' f (Id x) = Id (f x)
+errReadBind' m1 m2 r = case m1 r of Left e -> Left e
+                                    Right a -> m2 a r
+errReadBindTup' m1 m2 r = case m1 r of
+    Left e -> Left e
+    Right a -> case m2 a r of Left e -> Left e
+                              Right b -> Right (a,b)
+
+errReadBindTup2' :: ErrRead r e a -> (a -> ErrRead r e b) -> ErrRead r e (a,b)
+errReadBindTup2' m1 m2 = ErrRead $ \r -> case unErrRead m1 r of
+    Failure e -> Failure e
+    Success a -> case unErrRead (m2 a) r of Failure e -> Failure e
+                                            Success b -> Success (a,b)
+
+-- Here are functions where we do not infer the expected code, due to the order
+-- things are looked at.
+errReadAp' m1 m2 r = case m2 r of Left e -> case m1 r of Left e -> Left e
+                                                         Right _ -> Left e
+                                  Right x -> case m1 r of Left e -> Left e
+                                                          Right f -> Right (f x)
+contAp' :: (((a -> b) -> r) -> r) -> ((a -> r) -> r) -> ((b -> r) -> r)
+contAp' ca cb k = cb (\x -> ca (\f -> k (f x)))
 
 -- And here we use inspection-testing to check that these are indeed the
 -- definitions that GHC.JustDoIt created for us.
 
-inspect $ 'id             === 'id'
-inspect $ 'const          === 'const'
-inspect $ 'flip           === 'flip'
-inspect $ 'dup            === 'dup'
-inspect $ 'pair           === 'pair'
-inspect $ 'tripl          === 'tripl'
-inspect $ 'proj           === 'proj'
-inspect $ 'curry          === 'curry'
-inspect $ 'curryFlip      === 'curryFlip'
-inspect $ 'unit           === 'unit'
-inspect $ 'contBind       === 'contBind'
-inspect $ 'swapEither     === 'swapEither'
-inspect $ 'swapEitherCont === 'swapEitherCont'
-inspect $ 'absurd         === 'absurd'
-inspect $ 'mapId          === 'mapId'
+inspect $ 'id              === 'id'
+inspect $ 'const           === 'const'
+inspect $ 'flip            === 'flip'
+inspect $ 'dup             === 'dup'
+inspect $ 'pair            === 'pair'
+inspect $ 'tripl           === 'tripl'
+inspect $ 'proj            === 'proj'
+inspect $ 'curry           === 'curry'
+inspect $ 'curryFlip       === 'curryFlip'
+inspect $ 'unit            === 'unit'
+inspect $ 'contBind        === 'contBind'
+inspect $ 'contAp          === 'contAp'
+inspect $ 'swapEither      === 'swapEither'
+inspect $ 'swapEitherCont  === 'swapEitherCont'
+inspect $ 'absurd          === 'absurd'
+inspect $ 'mapId           === 'mapId'
+inspect $ 'errReadBind     === 'errReadBind'
+inspect $ 'errReadBindTup  === 'errReadBindTup'
+inspect $ 'errReadBindTup2 ==- 'errReadBindTup2' -- type variable order differences
+inspect $ 'errReadAp       === 'errReadAp'
 
 main :: IO ()
 main = putStrLn "☺"
